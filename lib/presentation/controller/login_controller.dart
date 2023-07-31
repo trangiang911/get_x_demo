f92219ba3ec59c0cce221e15f7f8cb3ecf25bbc7
trangiang911 <tbgiang191199@gmail.com>
2023-07-31 22:47:52 +0700

    commit firebase

---
+++ lib/presentation/controller/login_controller.dart
@@ -0,0 +1,5 @@
+import 'package:project_get_x/presentation/controller/base_controller.dart';
+
+class LoginController extends BaseController {
+
+}
---
+++ lib/presentation/view/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:project_get_x/presentation/controller/login_controller.dart';
+import 'package:project_get_x/presentation/view/base_layout_page.dart';
+
+class LoginPage extends BaseLayoutPage<LoginController> {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget buildView() {
+    return Container();
+  }
+}
